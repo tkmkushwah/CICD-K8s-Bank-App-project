aa52fc93bb639a7aea2fb784b056a7b2926592f4
Tikam Singh <tikamsingh1901900@gmail.com>
2025-09-02 21:31:15 +0530

    AWS Provision complete

--- EKS_Terraform/main.tf
+++ EKS_Terraform/main.tf
@@ -159,3 +159,60 @@ resource "aws_eks_node_group" "devopstkm" {
       source_security_group_ids=[aws_security_group.devopstkm_node_sg.id] //security group which allows inbound traffic
     }
 }
+
+resource "aws_iam_role" "devopstkm_cluster_role" {
+  name="devopstkm-cluster-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+//Attach the AmazonEKSClusterPolicy to the IAM Role
+resource "aws_iam_role_policy_attachment" "devopstkm_cluster_role_policy"{
+  role = aws_iam_role.devopstkm_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+resource "aws_iam_role" "devopstkm_node_group_role" {
+  name = "devopstkm-node-group-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+//Attach the required policies to the IAM Role
+resource "aws_iam_role_policy_attachment" "devopstkm_node_group_role_policy" {
+  role       = aws_iam_role.devopstkm_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+//Policy for the CNI plugin to manage networking resources on your behalf.
+resource "aws_iam_role_policy_attachment" "devopstkm_cni_policy" {
+  role       = aws_iam_role.devopstkm_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+//Policy to allow nodes to pull container images from Amazon ECR
+resource "aws_iam_role_policy_attachment" "devopstkm_node_group_registry_policy" {
+  role       = aws_iam_role.devopstkm_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
